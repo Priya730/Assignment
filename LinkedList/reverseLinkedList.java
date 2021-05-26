a3459043936eb4659e860011a2f55fedf71a95c9
Priya Srivastava <shivikapriya730@gmail.com>
2021-05-26 23:07:31 +0530

    Update reverseLinkedList.java

--- LinkedList/reverseLinkedList.java
+++ LinkedList/reverseLinkedList.java
@@ -12,7 +12,7 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         if(head == null || head.next == null) return head;
         ListNode prev = reverseList(head.next);
-        head.next.next = head.next;
+        head.next.next = head;
         head.next = null;
         
         return prev;
